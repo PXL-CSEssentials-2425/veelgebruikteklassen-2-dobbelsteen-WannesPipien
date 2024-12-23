b9ec214524206b4705b43c6857d49ad9697b750f
Wannes Pipien <wannes.pipien@student.pxl.be>
2024-12-23 11:56:20 +0100

    oefening af

---
+++ Dobbelen/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dobbelen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultTextBox.Clear();
+        }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            Random ds = new Random();
+            StringBuilder rs = new StringBuilder();
+            int result = 0;
+            int i = 1;
+            do
+            {
+                result = ds.Next(1, 7);
+                rs.AppendLine($"worp {i} is {result}");
+                i++;
+            } while (result != 6);
+            resultTextBox.Text = rs.ToString();
+        }
+    }
+}
